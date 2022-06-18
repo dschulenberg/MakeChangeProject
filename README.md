f8a8293d22b6d193f600635ab925ca29dafeab58
Daniel Schulenberg <dschumil@gmail.com>
2022-06-18 13:11:48 -0700

    Updated README and added all code lines.

--- README.md
+++ README.md
@@ -2,11 +2,22 @@
 
 
 ## Description
-
-
+This program will ask the user for two inputs. The first for the cost of the item and the second for 
+how much was given. The next step will determine whether or not the amount given was the exact change or if
+they did not give enough money through the use of a method. After passing this condition, the remainder, given
+-cost; is put through a while statement and passed through a series of if statements that will determine
+how much of of a specific currency should be given back. It starts with the highest currency available and pass 
+that remainder down the line. If it can met the condition, it will fall to the next if statement.The Other than
+variable remainder is changed during each if statement and a continue; is used to skip the rest of the code and
+filter the new remainder in the if statements.
+ the use of math, the amount remainder for pennies is different and uses the Math.round() feature as there wont
+ be any if statement following pennies and the number needs to be rounded up. The while loop continue to 
+ capture. Finally the output will be a printed line of what should be given back as a refund.
 
 ## Technologies used
-
+This can mainly be used in restaurants and banks that have face to face currency interactions. This could
+also apply for any online transactions where a customer paid too much and a refund was needed; ex:billing.
 
 
 ## What I learned
+I am still playing around with this code. I think ideally there is a way to wrap the if statements into a method and then pass the remainder and the currency; ten, five, one, etc... into it and that would really shorten the length of coding. This also taught me that there is a legitimate use of coding that has worldly practicalities.
--- src/com/skilldistillery/makechange/MakeChange.java
+++ src/com/skilldistillery/makechange/MakeChange.java
@@ -1,10 +1,123 @@
 package com.skilldistillery.makechange;
 
+import java.util.Scanner;
+
 public class MakeChange {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
 
+		System.out.println("How much was the item?");
+		double cost = scan.nextDouble();
+
+		System.out.println("How much money was given?");
+		double given = scan.nextDouble();
+
+		if (cost > given || cost == given) {
+			System.out.println(notEnough(cost, given));
+		}
+		System.out.println();
+		System.out.println("CHA CHING. The amount refunded is:");
+		System.out.println();
+		changeLeft(cost, given);
+
+		scan.close();
 	}
 
+	public static String notEnough(double cost, double given) {
+		if (cost > given) {
+			return		"You provided too little money. Please try again.";
+		} else {
+			return		"Thank you for providing the exact amount.";
+		}
+	}
+	
+
+	public static void changeLeft(double cost, double given) {
+		double remainder = given - cost, ten = 10, five = 5, one = 1, quarter = .25, dime = .10, nickle = .05, penny = .01;
+		while (remainder != 0 && remainder > 0) { 
+			double divide = Math.floor(remainder / ten);
+			if (divide >= 1) {
+				double leftover = remainder - ten * divide;
+				remainder = leftover;
+				if (divide == 1) {
+					System.out.println(divide + " ten dollar bill ");
+				} else {
+					System.out.println(divide + " ten dollar bills ");
+				}
+				continue; 						
+			} else if (remainder > five) {
+				divide = Math.floor(remainder / five);
+				if (divide >= 1) {
+					double leftover = remainder - five * divide;
+					remainder = leftover;
+					if (divide == 1) {
+						System.out.println(divide + " five dollar bill ");
+					} else {
+						System.out.println(divide + " five dollar bills ");
+					}
+					continue;
+				}
+			} else if (remainder > one) {
+				divide = Math.floor(remainder / one);
+				if (divide >= 1) {
+					double leftover = remainder - one * divide;
+					remainder = leftover;
+					if (divide == 1) {
+						System.out.println(divide + " one dollar bill");
+					} else {
+						System.out.println(divide + " one dollar bills ");
+					}
+					continue;
+				}
+			} else if (remainder > quarter) {
+				divide = Math.floor(remainder / quarter);
+				if (divide >= 1) {
+					double leftover = remainder - quarter * divide;
+					remainder = leftover;
+					if (divide == 1) {
+						System.out.println(divide + " quarter ");
+					} else {
+						System.out.println(divide + " quarters ");
+					}
+					continue;
+				}
+			} else if (remainder > dime) {
+				divide = Math.floor(remainder / dime);
+				if (divide >= 1) {
+					double leftover = remainder - dime * divide;
+					remainder = leftover;
+					if (divide == 1) {
+						System.out.println(divide + " dime ");
+					} else {
+						System.out.println(divide + " dimes");
+					}
+					continue;
+				}
+			} else if (remainder > nickle) {
+				divide = Math.floor(remainder / nickle);
+				if (divide >= 1) {
+					double leftover = remainder - nickle * divide;
+					remainder = leftover;
+					if (divide == 1) {
+						System.out.println(divide + " nickle ");
+					} else {
+						System.out.println(divide + " nickles");
+					}
+					continue;
+				}
+			} else if (remainder > penny || remainder > 0.001) {
+				divide = Math.round(remainder/penny);
+				if (divide >= 1) {
+					double leftover = remainder - penny * divide;
+					remainder = leftover;
+					if (divide == 1) {
+						System.out.println(divide + " penny ");
+					} else {
+						System.out.println(divide+ " pennies");
+					}
+				}
+			}
+		break;	
+		}
+	}
 }
